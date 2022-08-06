1ec2800f2a07909676bb81db8e6510f669d7de83
hyeonuk-choo <108967896+hyeonuk-choo@users.noreply.github.com>
2022-08-06 15:40:27 +0900

    id 표시 및 이전으로 버튼 생성

--- src/components/WorryDetail.jsx
+++ src/components/WorryDetail.jsx
@@ -1,16 +1,25 @@
-import Header from "../components/layout/Header";
-import Footer from "../components/layout/Footer";
-import Layout from "../components/layout/Layout";
-
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 const WorryDetail = () => {
+  let { id } = useParams();
+
   return (
     <>
-      <header />
-      <div>
-        <span> id : 01 </span>
-      </div>
+      <StDiv>
+        <span> id : {id} </span>
+        <button>이전으로</button>
+      </StDiv>
     </>
   );
 };
 
 export default WorryDetail;
+
+const StDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  margin: 20px auto;
+`;
