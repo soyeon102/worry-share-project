5955054addcda3b085a027b8b29365f4bba36f97
DESKTOP-KBMLAIH\jsi98 <sij7304@gmail.com>
2022-08-08 18:30:27 +0900

    리스트 페이지 get 구현(add delete 미완/css 틀어지는 이슈있음)

--- src/components/WorryList.jsx
+++ src/components/WorryList.jsx
@@ -2,9 +2,24 @@ import styled from "styled-components";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // axios import 합니다.
 
 const WorryList = () => {
   const navigate = useNavigate();
+  const [worries, setWorries] = useState(null);
+
+  const fetchWorries = async () => {
+    const { data } = await axios.get("http://localhost:3001/worries");
+    setWorries(data); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
+  };
+
+  useEffect(() => {
+    // effect 구문에 생성한 함수를 넣어 실행합니다.
+    fetchWorries();
+  }, []);
+
+  console.log(worries);
 
   return (
     <div
@@ -14,7 +29,9 @@ const WorryList = () => {
     >
       <StWorryList>
         <StListtitle>
-          Title
+          {worries?.map((worry) => (
+            <span key={worry.id}>{worry.title}</span>
+          ))}
           <IconButton
             color='primary'
             onClick={(e) => {
@@ -25,7 +42,12 @@ const WorryList = () => {
           </IconButton>
         </StListtitle>
 
-        <StListWriter>작성자:나</StListWriter>
+        <StListWriter>
+          작성자:{" "}
+          {worries?.map((worry) => (
+            <span key={worry.id}>{worry.user}</span>
+          ))}
+        </StListWriter>
       </StWorryList>
     </div>
   );
