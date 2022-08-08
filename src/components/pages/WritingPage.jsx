bbad2e0e9145343a51150e274417faf4bb5cefcc
DESKTOP-KBMLAIH\jsi98 <sij7304@gmail.com>
2022-08-08 15:56:27 +0900

    post기능 임시구현(커스텀버튼 적용시 무반응 이슈있습니다)

--- src/components/pages/WritingPage.jsx
+++ src/components/pages/WritingPage.jsx
@@ -2,35 +2,86 @@ import Layout from "../layout/Layout";
 import CommonInput from "../elements/CommonInput";
 import CommonButton from "../elements/CommonButton";
 import CommonTextArea from "../elements/CommonTextArea";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const WritingPage = () => {
-  const onsubmitHandler = (e) => {
-    e.preventDefault();
+  const [worry, setWorry] = useState({
+    user: "",
+    title: "",
+    content: "",
+  });
+
+  const [worries, setWorries] = useState(null);
+
+  const fetchWorries = async () => {
+    const { data } = await axios.get("http://localhost:3001/worries");
+    setWorries(data); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
   };
 
+  const onSubmitHandler = (worry) => {
+    axios.post("http://localhost:3001/worries", worry);
+  };
+
+  useEffect(() => {
+    // effect 구문에 생성한 함수를 넣어 실행합니다.
+    fetchWorries();
+  }, []);
+
+  const handleinputUser = (ev) => {
+    const { value } = ev.target;
+    setWorry({
+      ...worry,
+      user: value,
+    });
+  };
+
+  const handleinputTitle = (ev) => {
+    const { value } = ev.target;
+    setWorry({
+      ...worry,
+      title: value,
+    });
+  };
+
+  const handleinputContent = (ev) => {
+    const { value } = ev.target;
+    setWorry({
+      ...worry,
+      content: value,
+    });
+  };
   return (
     <Layout>
       <div>
-        <form onSubmit={onsubmitHandler}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmitHandler(worry);
+          }}
+        >
           <CommonInput
             text='이름을 입력해주세요.'
             id='Writer'
             labeltext='작성자'
+            onChange={handleinputUser}
           />
 
           <CommonInput
             text='제목을 입력해주세요.'
             id='Title'
             labeltext='제목'
+            onChange={handleinputTitle}
           />
 
           <CommonTextArea
             text='내용을 입력해주세요.'
             id='Content'
             labeltext='내용'
+            onChange={handleinputContent}
           />
 
-          <CommonButton
+          <button
             text='추가하기'
             size='100%'
             variant='contained'
